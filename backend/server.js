aa6e123c149be683067a5668c8afe4c206e728e1
raghavreddy-1106 <raghavreddy1106@gmail.com>
2026-09-15 00:26:59 +0530

    feat: add JWT authentication middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
+const testRoutes = require("./routes/testRoutes");
 
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api", testRoutes);
 
 
 
